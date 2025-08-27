464b2d4c2fd289aaa95a71405a1aba7c0b19211a
serenityux <thomas@serenidad.app>
2025-08-27 14:24:17 -0700

    now includes extensions

--- api/api/routes.go
+++ api/api/routes.go
@@ -14,8 +14,8 @@ func SetupRoutes(r *chi.Mux, srv *structs.Server) {
 	r.Post("/api/uploadGame", handlers.GameUploadHandler(srv)) // Probably required by vibecode..
 	r.Post("/uploadMiscFile", handlers.UploadMiscFileHandler(srv))
 	r.Post("/api/uploadMiscFile", handlers.UploadMiscFileHandler(srv))
-	r.Get("/misc-file/{fileId}", handlers.ServeMiscFileHandler(srv))
-	r.Get("/api/misc-file/{fileId}", handlers.ServeMiscFileHandler(srv))
+	r.Get("/misc-file/{fileId}.*", handlers.ServeMiscFileHandler(srv))
+	r.Get("/api/misc-file/{fileId}.*", handlers.ServeMiscFileHandler(srv))
 	r.Get("/play/{gameId}", handlers.MainGamePlayHandler(srv))
 	r.Get("/play/{gameId}/*", handlers.AssetsPlayHandler(srv))
 	r.Get("/removeGame/{gameId}", handlers.RemoveGameHandler(srv))
